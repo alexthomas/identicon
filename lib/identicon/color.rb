dc6dd9c181aed5f317d0773799fb39170aab7592
Alex Thomas <alex.c.thomas@gmail.com>
2016-06-03 12:33:15 +0100

    remove html color debug

--- lib/identicon/color.rb
+++ lib/identicon/color.rb
@@ -1,29 +1,28 @@
 module Identicon
-  
+
   class Color
-    
+
     attr_reader :html_color, :red, :blue, :green, :hue, :saturation, :lightness
-    
+
     def initialize(html_color)
       @html_color = html_color[1..-1]
-      puts "html color: #{@html_color}"
       to_rgb
     end
-    
+
     def to_rgb
       rgb_array = html_color.scan(/../)
       @red ||= rgb_array[0].to_i(16)
       @green ||= rgb_array[1].to_i(16)
       @blue ||= rgb_array[2].to_i(16)
     end
-    
+
     def to_hsl
-      
+
     end
-    
+
     def hsl_to_rgb
-      
+
     end
-    
+
   end
 end
